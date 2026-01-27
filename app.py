fbf9f695e9870820c896dccad5ebf264c3a575d2
Rahul Gupta <rahulx106@gmail.com>
2026-01-28 01:25:53 +0530

    link added for backend api

--- app.py
+++ app.py
@@ -7,14 +7,8 @@ warnings.filterwarnings("ignore")
 # =============================
 # CONFIG
 # =============================
-USE_LOCAL = False
 
-API_BASE = (
-    "http://127.0.0.1:8000"
-    if USE_LOCAL
-    else "https://movie-rec-466x.onrender.com"
-)
-
+API_BASE = "https://movie-recommendation-system-wh20.onrender.com" or "http://127.0.0.1:8000"
 TMDB_IMG = "https://image.tmdb.org/t/p/w500"
 
 st.set_page_config(
